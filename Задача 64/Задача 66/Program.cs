576dd3b93dd91039b9e9247a0edd050efdc67084
Medelig <elen-kruk@mail.ru>
2023-12-08 16:04:24 +0300

    Все задачи

---
+++ Задача 64/Задача 66/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт
+// сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+Console.Write("Введите значение М ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (M<=N)
+{
+    int sum = Sum(M,N);
+    Console.WriteLine($"Сумма натуральных чисел от {M} до {N} равна {sum}");
+}
+else
+{
+    Console.WriteLine("Ошибка. Проверьте введенные числа");
+}
+int Sum(int start, int end)
+{
+    if (start > end)
+    {
+        return 0;
+    }
+    else
+    {
+        return start + Sum(start + 1, end);
+    }
+}
+
---
+++ Задача 64/Задача 66/Задача 68/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+Console.Write("Введите значение m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение n ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = AkkerFunc(m, n);
+Console.WriteLine($"Результат функции Аккермана для m={m} и n={n} {result}");
+int AkkerFunc(int m, int n)
+{ 
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n==0)
+    {
+        return AkkerFunc(m-1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        return AkkerFunc(m-1, AkkerFunc(m, n-1));
+    }
+    else
+    {
+        return 0;
+    }
+}
